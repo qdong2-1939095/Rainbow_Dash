7614674c32c3defe71444f1a7f7a0e6aff876f15
Felix Dong <oncwnuAycLZJnB-NrryU8vMJQwKI@git.weixin.qq.com>
2022-04-02 16:35:31 -0700

    modify canvas func

--- canvas.py
+++ canvas.py
@@ -6,26 +6,19 @@ def load_canvas():
     app.geometry("1080x720")
     canvas = Canvas(app, width=1080, height=720, bg='white')
     canvas.pack(pady=20)
-    # canvas = Canvas(app, bg='red')
-    # canvas.pack(anchor='nw', fill='both', expand=1)
 
-def get_x_and_y(event):
-    global lasx, lasy
-    lasx, lasy = event.x, event.y
+lasx = 540
+lasy =  360
 
 def _from_rgb(rgb):
     """translates an rgb tuple of int to a tkinter friendly color code
     """
-    return "#%02x%02x%02x" % rgb  
-
-def draw_smth(event):
-    global lasx, lasy
-    canvas.create_line((lasx, lasy, event.x, event.y),
-                      fill=_from_rgb((250, 200, 200)),
-                      width=2)
-    lasx, lasy = event.x, event.y
+    return "#%02x%02x%02x" % rgb
 
 def draw_line(x_offset, y_offset, r, g, b):
-    canvas.bind("<Button-1>", get_x_and_y)
-    canvas.bind("<B1-Motion>", draw_smth)
+    global lasx, lasy
+    canvas.create_line((lasx, lasy, x_offset, y_offset),
+                      fill=_from_rgb((r, g, b)),
+                      width=2)
+    lasx, lasy = lasx + x_offset, lasy + y_offset
 # app.mainloop()
